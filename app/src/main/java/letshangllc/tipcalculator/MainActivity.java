5bd72a0173a3675cf51bf157e3209c069571ef24
Carl Burnham <cburnham@letshang.us>
2015-12-25 19:56:45 -0800

    found views in main activity

--- app/src/main/java/letshangllc/tipcalculator/MainActivity.java
+++ app/src/main/java/letshangllc/tipcalculator/MainActivity.java
@@ -2,6 +2,7 @@ package letshangllc.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText et_billTotal = (EditText) findViewById(R.id.et_billTotal);
+        EditText et_tipPercent = (EditText) findViewById(R.id.et_tipPercent);
+        EditText et_taxPercent = (EditText) findViewById(R.id.et_taxPercent);
+        EditText et_numPeople = (EditText) findViewById(R.id.et_numPeople);
+        EditText et_totalCost = (EditText) findViewById(R.id.et_totalCost);
+        EditText et_costPerPerson = (EditText) findViewById(R.id.et_costPerPerson);
+        
     }
 }
